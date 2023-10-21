18e74e5d2bf752449dcb34b9d7d6702a6912d1c6
atharvabhide <atharvabhide91@gmail.com>
2023-10-21 23:27:20 +0530

    chore: remove test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-customer_email = "inwardsynctest1@test.com"
-
-response = requests.get(f"http://127.0.0.1:8080/get_id/{customer_email}", headers={'Content-Type': 'application/json'})
-
-print(response.status_code)
-print(response.json())
